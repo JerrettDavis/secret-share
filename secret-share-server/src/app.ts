a214de15dea5450c539b0bb7a5427870b5a72353
JD Davis <mxjerrett@gmail.com>
2024-11-19 23:11:25 -0600

    fix: updated cors and devlopment docker compsoe

--- secret-share-server/src/app.ts
+++ secret-share-server/src/app.ts
@@ -1,34 +1,39 @@
-import express, {Application, NextFunction, Request, Response} from 'express';
-import cors from 'cors';
-import favicon from 'serve-favicon';
-import path from 'path';
-import {logger} from '@middleware/logger';
-import routes from './routes';
-import http from 'http';
-import {appState, ServiceState} from "src/appState";
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import favicon from "serve-favicon";
+import path from "path";
+import { logger } from "@middleware/logger";
+import routes from "./routes";
+import http from "http";
+import { appState, ServiceState } from "src/appState";
 import errorMiddleware from "@middleware/errorMiddleware";
-
+import process from "node:process";
 const app: Application = express();
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
+const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5001", // Replace with client's Docker hostname or IP
+    methods: ["GET", "POST", "PUT", "DELETE"], // Adjust based on your API's methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Include any custom headers
+    credentials: true, // Allow cookies if needed
+};
+app.use(cors(corsOptions));
+app.options("*", cors());
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger);
-app.use('/', routes);
+app.use("/", routes);
 app.use((_: Request, res: Response, next: NextFunction) => {
     res.status(404).json({
-        status: 'error',
-        message: 'Resource not found'
+        status: "error",
+        message: "Resource not found",
     });
     next();
 });
 app.use(errorMiddleware);
-
 const startServer = (app: Application): Promise<http.Server> => {
     const PORT = process.env.PORT || 5000;
     appState.ExpressServer = ServiceState.STARTING;
-    console.log('<API> Starting server...');
+    console.log("<API> Starting server...");
     return new Promise((resolve, reject) => {
         try {
             const server = app.listen(PORT, () => {
@@ -37,16 +42,15 @@ const startServer = (app: Application): Promise<http.Server> => {
                 resolve(server);
             });
         } catch (e) {
-            console.error('<API> Server startup error:', e);
+            console.error("<API> Server startup error:", e);
             appState.ExpressServer = ServiceState.ERROR;
             reject(e);
             throw e;
         }
     });
 };
-
 const stopServer = (server: http.Server): Promise<void> => {
-    console.log('<API> Stopping server...');
+    console.log("<API> Stopping server...");
     appState.ExpressServer = ServiceState.STOPPING;
     return new Promise((resolve, _) => {
         server.close(() => {
@@ -55,5 +59,4 @@ const stopServer = (server: http.Server): Promise<void> => {
         });
     });
 };
-
-export default {startServer, stopServer, app};
+export default { startServer, stopServer, app };
--- secret-share-server/src/startup.ts
+++ secret-share-server/src/startup.ts
@@ -1,33 +1,36 @@
-import 'src/config/dotenv';
-import app from 'src/app';
-import {connectToDatabase, stopMongoDB} from 'src/config/db';
-import {connectToRabbitMQ, stopRabbitMQ} from 'src/config/rabbitmq';
+import "src/config/dotenv";
+import app from "src/app";
+import { connectToDatabase, stopMongoDB } from "src/config/db";
+import { connectToRabbitMQ, stopRabbitMQ } from "src/config/rabbitmq";
 import http from "http";
-
 const handleShutdown = (server: http.Server) => {
     const shutdown = async () => {
-        console.log('Shutting down...');
-        await Promise.all([stopMongoDB(), stopRabbitMQ(), app.stopServer(server)]);
+        console.log("Shutting down...");
+        await Promise.all([
+            stopMongoDB(),
+            stopRabbitMQ(),
+            app.stopServer(server),
+        ]);
         process.exit(0);
     };
-
-    process.on('SIGINT', async () => await shutdown());
-    process.on('SIGTERM', async () => await shutdown());
+    process.on("SIGINT", async () => await shutdown());
+    process.on("SIGTERM", async () => await shutdown());
 };
-
 const startup = async () => {
-    console.log('<APP> Starting up...');
-
+    console.log("<APP> Starting up...");
     try {
-        await Promise.all([connectToDatabase(process.env.MONGO_URI || ''), connectToRabbitMQ()]);
+        await Promise.all([
+            connectToDatabase(process.env.MONGO_URI || ""),
+            connectToRabbitMQ(),
+        ]);
+        console.log("<APP> Connected to depedencies. Starting API...");
         const server = await app.startServer(app.app);
         handleShutdown(server);
-
-        console.log('<APP> App startup completed');
+        console.log("<APP> App startup completed");
     } catch (err) {
-        console.error('<APP> Startup error:', err);
+        console.error("<APP> Startup error:", err);
         throw err;
     }
 };
-
 export default startup;
+
